cd8786f4bb03a90feffd47832488e4258ae53b1e
Miguel A. Alonso <migalons74@gmail.com>
2017-06-27 00:43:00 +0200

    Add first given step

--- test/step_definitions/Given_steps.js
+++ test/step_definitions/Given_steps.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const {defineSupportCode} = require('cucumber');
 
+const {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Given, When, Then}) {
 
-    Given('a user at login screen', function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
+    Given('a user at login screen', function () {
+        this.db.filter((user) => true);
     });
 
     Given('a user with valid credentials at login page', function (callback) {
