112165786281ff6e7e5bf0828ab4bd9d150edc9e
Miguel A. Alonso <migalons74@gmail.com>
2017-06-27 01:46:41 +0200

    Implemented givel step definitions

--- test/step_definitions/Given_steps.js
+++ test/step_definitions/Given_steps.js
@@ -5,18 +5,19 @@ const {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Given, When, Then}) {
 
-    Given('a user at login screen', function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
+    Given('a user at login screen', function () {
+        this.db.filter((user) => true);
+        log.info("Selected user: " + this.db.user.credentials.username);
     });
 
     Given('a user with valid credentials at login page', function () {
         this.db.filter((user) => true);
+        log.info("Selected user: " + this.db.user.credentials.username);
     });
 
-    Given('a user with current accounts', function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
+    Given('a user with current accounts', function () {
+        this.db.filter((user) => user.products.accounts);
+        log.info("Selected user: " + this.db.user.credentials.username);
     });
 
 });
