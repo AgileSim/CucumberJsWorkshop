9b02cc0bc6758bb8c48470d1d3dcfbeddeae1580
Miguel A. Alonso <migalons74@gmail.com>
2017-06-26 22:59:01 +0200

    Add step definitions with pending status

---
+++ test/step_definitions/global_position.js
@@ -0,0 +1,22 @@
+var {defineSupportCode} = require('cucumber');
+
+
+defineSupportCode(function ({Given, When, Then}) {
+
+
+    Given('a user with current accounts', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+    });
+
+    When('he access his global position', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+    });
+
+    Then('he sould see his accounts each with this information:', function (table, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+    });
+
+});
--- test/step_definitions/login_steps.js
+++ test/step_definitions/login_steps.js
@@ -1,12 +1,32 @@
 var {defineSupportCode} = require('cucumber');
 
 
-defineSupportCode(function({Given}) {
+defineSupportCode(function({Given, When, Then}) {
 
     Given('a user at login screen', function (callback) {
 
-        throw "Custom error";
+        callback(null, 'pending');
 
+    });
+
+    Given('a user with valid credentials at login page', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+    });
+
+    When('he introduces his credentials', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+    });
+
+    When('he introduces wrong credentials', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+    });
+
+    Then('he should view his global position', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
     });
 
 });
