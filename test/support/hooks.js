a8f399a4fd6e3739acc5a2ef14d38cabae4183df
Miguel A. Alonso <migalons74@gmail.com>
2017-06-26 23:34:36 +0200

    Added World constructor and hooks

--- test/support/hooks.js
+++ test/support/hooks.js
@@ -2,4 +2,13 @@ var {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Before, After}) {
 
+    Before(function () {
+        return this.driver
+            .init()
+            .url(this.baseURL)
+    });
+
+    After(function() {
+        return this.driver.end();
+    });
 });
--- test/support/world.js
+++ test/support/world.js
@@ -1,10 +1,20 @@
 require('chromedriver')
 // var seleniumWebdriver = require('selenium-webdriver');
 var {defineSupportCode} = require('cucumber');
+let webdriverio = require('webdriverio');
+let options = {
+                desiredCapabilities: {
+                    browserName: 'chrome',
+                    chromeOptions: {
+                        binary: '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
+                    }
+                }
+              };
 
 
 function CustomWorld() {
-
+    this.baseURL = "http://35.156.130.109:4200/";
+    this.driver = webdriverio.remote(options);
 }
 
 defineSupportCode(function({setWorldConstructor}) {
